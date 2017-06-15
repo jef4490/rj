c2b08026d5f402b1e5a48ab5248eb59ff9762c70
jef4490 <jeffrey.hechler@gmail.com>
2017-06-14 20:57:09 -0400

    styling, gig content

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Parallax } from 'react-parallax'
 import logo from './logo.svg';
 import './App.css';
-import bgimg from './img/stockparallax.jpg';
+import bgimg from './img/RansomJonesLogo.jpg';
 import Navbar from './components/navbar'
 
 class App extends Component {
@@ -10,20 +10,77 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Ransom Jones</h2>
+          <h1>Ransom Jones</h1>
+          <h3>South East Michigan's Ultimate Classic Rock Party Band</h3>
         </div>
-        <Navbar />
+        {/* <Navbar /> */}
         <Parallax bgImage={bgimg} strength={250}>
-          <img src={logo} className="App-logo" alt="logo" />
-          <div style={{height: 800}}>
-            <div style={{height: '15em', paddingTop: '3em', background: '#fff', width: '20em'}}>
-              Latest Gigs
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <div style={{height: 1000}}>
+            <div style={{opacity: 0.7, paddingTop: '3em', marginLeft: '1em', background: '#fff', width: '30em', paddingBottom: '1em', borderRadius: '6px'}}>
+              <h2>Upcoming Shows</h2>
+              <ul style={{listStyleType: 'none'}}>
+                <li>
+                  <h4>Weber's Inn</h4>
+                    <ul style={{listStyleType: 'none'}}>
+                      <li>
+                        Saturday, July 1st - 9PM
+                      </li>
+                      <li>
+                        3050 Jackson Rd, Ann Arbor, MI 48103
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <h4>Private Event</h4>
+                    <ul style={{listStyleType: 'none'}}>
+                      <li>
+                        Saturday, July 29th - 6PM
+                      </li>
+                  </ul>
+                </li>
+                <li>
+                  <h4>Private Event</h4>
+                  <ul style={{listStyleType: 'none'}}>
+                    <li>
+                      Saturday, September 2nd - 6PM
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <h4>Private Event</h4>
+                  <ul style={{listStyleType: 'none'}}>
+                    <li>
+                      Saturday, July 29th - 6PM
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <h4>Private Event</h4>
+                  <ul style={{listStyleType: 'none'}}>
+                    <li>
+                      Saturday, October 28th - 8PM
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <h4>Live</h4>
+                  <ul style={{listStyleType: 'none'}}>
+                    <li>
+                      Friday, January 12th - 6PM
+                    </li>
+                </ul>
+              </li>
+
+              </ul>
             </div>
           </div>
+
+
         </Parallax>
 
         <p className="App-intro">
-          Copywrite Ransom Jones 2017
+          &copy; Ransom Jones 2017
         </p>
       </div>
     );
