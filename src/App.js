7a11504729dce54bcc53d4fac35abd03ae2329e0
jef4490 <jeffrey.hechler@gmail.com>
2018-01-23 21:16:14 -0500

    rollback

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Parallax } from 'react-parallax'
 import logo from './logo.svg';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
@@ -11,18 +11,19 @@ import bgimg from './img/RansomJonesLogo.jpg';
 import Navbar from './components/navbar'
 import Upcoming from './components/upcoming'
 import Contact from './components/contact'
+import Songs from './components/songs'
 import Bio from './components/bio'
 import Social from './components/social'
 
 class Gigs extends Component {
     render() {
-        return <Upcoming styleProp={{opacity: 0.7, paddingTop: '3em', marginLeft: '1em', background: '#fff', width: '30em', paddingBottom: '1em', borderRadius: '6px'}} />
+        return <Upcoming styleProp={{opacity: 0.7, paddingTop: '1em', marginLeft: '1em', background: '#fff', width: '30em', paddingBottom: '1em', borderRadius: '6px'}} />
     }
 }
 
 class Shows extends Component {
     render() {
-        return <Upcoming styleProp={{opacity: 0.7, paddingTop: '3em', margin: 'auto', background: '#fff', width: '50em', paddingBottom: '1em', borderRadius: '6px'}} />
+        return <Upcoming styleProp={{opacity: 0.7, paddingTop: '1em', margin: 'auto', background: '#fff', width: '36em', paddingBottom: '1em', borderRadius: '6px'}} />
     }
 }
 
@@ -43,11 +44,11 @@ class App extends Component {
               <Route exact path='/contact' component={Contact} />
               <Route exact path='/band' component={Bio} />
               <Route exact path='/shows' component={Shows} />
-
+              <Route exact path='/songs' component={Songs} />
             </div>
           </Parallax>
           <p className="App-intro" style={{fontSize: 12}}>
-            &copy; Ransom Jones 2017
+            &copy; Ransom Jones 2018
           </p>
         </div>
       </Router>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,6 +9,7 @@ const NavBar = () => {
       <span><Link to="/">Home</Link></span>
       <span><Link to="/band">Band</Link></span>
       <span><Link to="/shows">Shows</Link></span>
+      <span><Link to="/songs">Songs</Link></span>
       <span><Link to="/contact">Contact</Link></span>
     </div>
   )
