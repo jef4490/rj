675f709ac1278e7f5e2da8e1a3aeb12308ccd3c0
jef4490 <jeffrey.hechler@gmail.com>
2019-01-12 14:54:51 -0500

    update shows

--- src/components/upcoming.js
+++ src/components/upcoming.js
@@ -5,31 +5,16 @@ const Upcoming = (props) => {
       <div style={props.styleProp}>
         <h2>Upcoming Shows</h2>
         <ul style={{listStyleType: 'none'}}>
-          <li className="show-header">
-            <p>07.17.18</p>
-            <h4><a target="_blank" href="https://chelseafestivals.com/soundsights/schedule/">Chelsea Sights & Sounds</a></h4>
-            <ul>
-              <li>
-                – 6:30 PM
-              </li>
-              <li>
-                East Middle Street Stage
-              </li>
-              <li>
-                Chelsea, MI 48118
-              </li>
-            </ul>
-          </li>
 
           <li className="show-header">
-            <p>07.28.18</p>
-            <h4><a target="_blank" href="http://www.hellsaloon.com/">Hell Saloon</a></h4>
+            <p>01.19.19</p>
+            <h4><a target="_blank" href="http://www.zukeylaketavern.com/">Zukey Lake Tavern</a></h4>
             <ul>
               <li>
-                – 9:00 PM
+                – 8:00 PM
               </li>
               <li>
-                4095 Patterson Lake Rd.
+                5011 Girard Dr
               </li>
               <li>
                 Pinckney, MI 48169
@@ -37,16 +22,8 @@ const Upcoming = (props) => {
             </ul>
           </li>
 
-          <li className="show-header">
-            <p>08.18.18</p>
-            <h4><a target="_blank" href="http://www.zukeylaketavern.com/">Private Event</a></h4>
-            <ul>
-            </ul>
-          </li>
-
-
         <li className="show-header">
-          <p>09.28.18</p>
+          <p>02.09.19</p>
           <h4><a target="_blank" href="http://www.hellsaloon.com/">Hell Saloon</a></h4>
           <ul>
             <li>
@@ -62,7 +39,23 @@ const Upcoming = (props) => {
         </li>
 
         <li className="show-header">
-          <p>11.10.18</p>
+          <p>02.22.19</p>
+          <h4><a target="_blank" href="http://www.zukeylaketavern.com/">Zukey Lake Tavern</a></h4>
+          <ul>
+            <li>
+              – 8:00 PM
+            </li>
+            <li>
+              5011 Girard Dr
+            </li>
+            <li>
+              Pinckney, MI 48169
+            </li>
+          </ul>
+        </li>
+
+        <li className="show-header">
+          <p>03.02.19</p>
           <h4><a target="_blank" href="http://www.hellsaloon.com/">Hell Saloon</a></h4>
           <ul>
             <li>
@@ -73,6 +66,38 @@ const Upcoming = (props) => {
             </li>
             <li>
               Pinckney, MI 48169
+            </li>
+          </ul>
+        </li>
+
+        <li className="show-header">
+          <p>03.29.19</p>
+          <h4><a target="_blank" href="http://www.zukeylaketavern.com/">Zukey Lake Tavern</a></h4>
+          <ul>
+            <li>
+              – 8:00 PM
+            </li>
+            <li>
+              5011 Girard Dr
+            </li>
+            <li>
+              Pinckney, MI 48169
+            </li>
+          </ul>
+        </li>
+
+        <li className="show-header">
+          <p>05.10.19</p>
+          <h4><a target="_blank" href="http://livea2.com/">LIVE</a></h4>
+          <ul>
+            <li>
+              – 6:00 PM
+            </li>
+            <li>
+              102 S 1st St, Ann Arbor, MI 48104
+            </li>
+            <li>
+              Ann Arbor, MI 48103
             </li>
           </ul>
         </li>
