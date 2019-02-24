0644d56fd471c96b0ddb4859b14857f98e304207
jef4490 <jeffrey.hechler@gmail.com>
2019-02-24 15:49:59 -0500

    update dates

--- src/components/upcoming.js
+++ src/components/upcoming.js
@@ -1,106 +1,35 @@
 import React from 'react';
 
+import { Gigs } from '../constants/gigs';
+
 const Upcoming = (props) => {
+
+  let formattedGigs = Gigs.map((gig, idx) => {
+    return(
+      <li className="show-header" key={idx}>
+        <p>{gig.date}</p>
+        <h4><a target="_blank" href={gig.url}>{gig.venue}</a></h4>
+        <ul>
+          <li>
+            – {gig.time}
+          </li>
+          <li>
+            {gig.address1}
+          </li>
+          <li>
+            {gig.address2}
+          </li>
+        </ul>
+      </li>
+    )
+  })
+
   return (
       <div style={props.styleProp}>
         <h2>Upcoming Shows</h2>
         <ul style={{listStyleType: 'none'}}>
 
-          <li className="show-header">
-            <p>01.19.19</p>
-            <h4><a target="_blank" href="http://www.zukeylaketavern.com/">Zukey Lake Tavern</a></h4>
-            <ul>
-              <li>
-                – 8:00 PM
-              </li>
-              <li>
-                5011 Girard Dr
-              </li>
-              <li>
-                Pinckney, MI 48169
-              </li>
-            </ul>
-          </li>
-
-        <li className="show-header">
-          <p>02.09.19</p>
-          <h4><a target="_blank" href="http://www.hellsaloon.com/">Hell Saloon</a></h4>
-          <ul>
-            <li>
-              – 9:00 PM
-            </li>
-            <li>
-              4095 Patterson Lake Rd.
-            </li>
-            <li>
-              Pinckney, MI 48169
-            </li>
-          </ul>
-        </li>
-
-        <li className="show-header">
-          <p>02.22.19</p>
-          <h4><a target="_blank" href="http://www.zukeylaketavern.com/">Zukey Lake Tavern</a></h4>
-          <ul>
-            <li>
-              – 8:00 PM
-            </li>
-            <li>
-              5011 Girard Dr
-            </li>
-            <li>
-              Pinckney, MI 48169
-            </li>
-          </ul>
-        </li>
-
-        <li className="show-header">
-          <p>03.02.19</p>
-          <h4><a target="_blank" href="http://www.hellsaloon.com/">Hell Saloon</a></h4>
-          <ul>
-            <li>
-              – 9:00 PM
-            </li>
-            <li>
-              4095 Patterson Lake Rd.
-            </li>
-            <li>
-              Pinckney, MI 48169
-            </li>
-          </ul>
-        </li>
-
-        <li className="show-header">
-          <p>03.29.19</p>
-          <h4><a target="_blank" href="http://www.zukeylaketavern.com/">Zukey Lake Tavern</a></h4>
-          <ul>
-            <li>
-              – 8:00 PM
-            </li>
-            <li>
-              5011 Girard Dr
-            </li>
-            <li>
-              Pinckney, MI 48169
-            </li>
-          </ul>
-        </li>
-
-        <li className="show-header">
-          <p>05.10.19</p>
-          <h4><a target="_blank" href="http://livea2.com/">LIVE</a></h4>
-          <ul>
-            <li>
-              – 6:00 PM
-            </li>
-            <li>
-              102 S 1st St, Ann Arbor, MI 48104
-            </li>
-            <li>
-              Ann Arbor, MI 48103
-            </li>
-          </ul>
-        </li>
+        {formattedGigs}
 
       </ul>
     </div>
---
+++ src/constants/gigs.js
@@ -0,0 +1,26 @@
+export const Gigs = [
+  {
+    venue: "Hell Saloon",
+    date: "03.02.19",
+    time: "9:00 PM",
+    address1: "4095 Patterson Lake Rd.",
+    address2: "Pinckney, MI 48169",
+    url: "http://www.hellsaloon.com/"
+  },
+  {
+    venue: "Zukey Lake Tavern",
+    date: "03.29.19",
+    time: "8:00 PM",
+    address1: "5011 Girard Dr",
+    address2: "Pinckney, MI 48169",
+    url: "http://www.zukeylaketavern.com/"
+  },
+  {
+    venue: "LIVE",
+    date: "05.10.19",
+    time: "6:00 PM",
+    address1: "102 S 1st St, Ann Arbor, MI 48104",
+    address2: "Ann Arbor, MI 48103",
+    url: "http://livea2.com/"
+  }
+]
