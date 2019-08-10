f116ee494d5e7902c588757c8aff3fd92d488171
jef4490 <jeffrey.hechler@gmail.com>
2019-08-10 10:18:00 -0400

    Styling fixes

--- src/components/songs.js
+++ src/components/songs.js
@@ -9,18 +9,14 @@ const Songs = () => {
 
   var setListFormatted = SetList.map((songsPairing) => {
     if(songsPairing[0] == "Song"){
-      return <span className="song-entry" style={{justifyContent: 'space-around'}}>
-        <h3 style={{display: 'inline-block', textDecoration: 'underline'}}>{songsPairing[0]}</h3> 
-        <h3 style={{display: 'inline-block', textDecoration: 'underline'}}>{songsPairing[1]}</h3>
+      return <span id="song-header-container" className="song-entry">
+        <h3 className="song-header">{songsPairing[0]}</h3> 
+        <h3 className="song-header">{songsPairing[1]}</h3>
       </span>
     }
     return <span className="song-entry"><h4 className="songDisplay">{songsPairing[0]}</h4><span className="song-entry-spacer"></span><h4 className='artistDisplay'>{songsPairing[1]}</h4></span>
   })
 
-  var setListReFormatted = <span className=''><h3 style={{display: 'inline-block', float: 'left'}}>Song</h3> <h3 style={{display: 'inline-block', float: 'right'}}>Artist</h3></span> + setListFormatted
-
-  // style={{padding: '1em', opacity: 0.7, background: '#fff', position: 'relative', minWidth: '90vw', margin: 'auto', borderRadius: '6px'}}
-  // style={{padding: '1em', opacity: 0.7, background: '#fff', position: 'relative', minWidth: '90vw', margin: 'auto', borderRadius: '6px', marginTop: '3em'}}
   return (
     <div id="songs-container">
       <div>
